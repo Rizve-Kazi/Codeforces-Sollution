45be46d5d6a2cc3508b6106e73309199bfd270ab
rizve_kazi <168621934+Rizve-Kazi@users.noreply.github.com>
2025-10-09 23:38:25 +0600

    Create  Ebony and Ivory.cpp

---
+++ Ebony and Ivory.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+  
+  int a,b,c;
+  cin>>a>>b>>c;
+
+  for(int i=0;i<=c;i++)
+  {
+    for(int j=0;j<=c;j++)
+    {
+       if((a*i)+(b*j)==c)
+       {
+          cout<<"Yes"<<endl;
+          return 0;
+       }
+    }
+  }
+  cout<<"No"<<endl;
+
+}
